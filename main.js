aeecefc2a4815b85313ae3e53fc0be8b631176ea
aboutmydreams <aboutmydreams@163.com>
2022-10-15 16:33:41 +0800

    fix: update version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Contract Scan
 // @name:zh-cn   合约扫描器
-// @version      1.0.4
+// @version      1.0.5
 // @namespace    https://github.com/DAOBuidler/contractscan
 // @homepage     https://github.com/DAOBuidler/contractscan
 // @author       Diven@BuidlerDAO Exqlnet@BuidlerDAO
